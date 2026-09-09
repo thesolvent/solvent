0c2c5c6a50ccb04fdca422595fd75933ae66b3b7
svssathvik7 <svssathvik77@gmail.com>
2026-09-09 16:14:35 +0530

    feat(contracts): add zero-inventory direct cross-chain settlement

---
+++ contracts/src/interfaces/IWETH.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IWETH is IERC20 {
+    function withdraw(uint256 amount) external;
+}
